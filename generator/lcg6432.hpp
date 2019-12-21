0722eba5729b45c241c54f6cd464250e77adaea0
Xuanyao Fong <xuanyao.fong@gmail.com>
2019-12-21 23:31:02 +0800

    Formatted lcg6432 kernel code

--- generator/lcg6432.hpp
+++ generator/lcg6432.hpp
@@ -1,3 +1,4 @@
+const char * lcg6432_prng_kernel = R"EOK(
 /**
 @file
 
@@ -78,3 +79,4 @@ Generates a random double using lcg6432 RNG. Generated using only 32 random bits
 @param state State of the RNG to use.
 */
 #define lcg6432_double2(state) (lcg6432_uint(state)*LCG6432_DOUBLE2_MULTI)
+)EOK";
