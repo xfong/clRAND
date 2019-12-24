bc4568ae8c39356178c6f6f6ee571b734f4bd8e5
Xuanyao Fong <xuanyao.fong@gmail.com>
2019-12-24 17:17:12 +0800

    Refine template for examples

---
+++ examples/util.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string>
+#include <unistd.h>
+#include <getopt.h>
+
+#define __CL_ENABLE_EXCEPTIONS
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+
+#if defined(__APPLE__) || defined(__MACOSX)
+    #include <OpenCL/cl.h>
+#else
+    #include <CL/cl.h>
+#endif
+
+typedef struct oclStruct__ {
+    cl_platform_id   target_platform;
+    cl_device_id     target_device;
+    cl_context       ctx;
+    cl_command_queue queue;
+} oclStruct;
+
+// Function to select a particular GPU in all GPUs found
+int chooseDeviceInPlatforms(int gpu_number, oclStruct* oclStructure);
+// Consolidate information about selected GPU into a data structure
+int makeOclStruct(int argc, char **argv, oclStruct* oclStructure);
+// Output information about selected GPU
+int printOclStructInfo(oclStruct* ComputeStructure);
