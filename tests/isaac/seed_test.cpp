084c408ef82b5af3a4cf1b727b09ebd6e0ca8d9c
Xuanyao Fong <xuanyao.fong@gmail.com>
2020-01-16 17:42:58 +0800

    Adding tests for isaac prng

--- tests/isaac/seed_test.cpp
+++ tests/isaac/seed_test.cpp
@@ -1,6 +1,38 @@
-#include ../../src/clPRNG.hpp
-#include utils.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "../../src/clrand.hpp"
+#include "utils.h"
 
-int main() {
-    return 0
+#define __CL_ENABLE_EXCEPTIONS
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+
+#if defined(__APPLE__) || defined(__MACOSX)
+    #include <OpenCL/cl.h>
+#else
+    #include <CL/cl.h>
+#endif
+
+int main(int argc, char **argv) {
+    cl_event          event = NULL;
+    cl_int            err = -1;
+
+    oclStruct* tmpStructPtr = (oclStruct*) malloc(sizeof(oclStruct));
+    int res = makeOclStruct(argc, argv, tmpStructPtr);
+    if (res != 0) {
+        fprintf(stderr,"Unable to create oclStruct!\n");
+        return res;
+    }
+    res = printOclStructInfo(tmpStructPtr);
+    if (res != 0) {
+        fprintf(stderr,"Unable to print information about oclStruct!\n");
+        return res;
+    }
+
+    clRAND* test = clrand_create_stream();
+    clrand_initialize_prng(test, (*tmpStructPtr).target_device, "tinymt32");
+    err = clrand_set_prng_precision(test, "uint");
+    free(tmpStructPtr);
+    return res;
 }
