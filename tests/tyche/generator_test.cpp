14df4bd8290327b838d98a3fe0c770c3369fad88
Xuanyao Fong <xuanyao.fong@gmail.com>
2020-01-19 23:30:03 +0800

    Added tests for tyche prngs

---
+++ tests/tyche/generator_test.cpp
@@ -0,0 +1,187 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "../../src/clrand.hpp"
+#include "utils.h"
+
+#define __CL_ENABLE_EXCEPTIONS
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+
+#if defined(__APPLE__) || defined(__MACOSX)
+    #include <OpenCL/cl.h>
+#else
+    #include <CL/cl.h>
+#endif
+
+#define TYCHE_ROT(a,b) (((a) << (b)) | ((a) >> (32 - (b))))
+
+#define tyche_ulong(state) (tyche_advance(&state), state.res)
+void tyche_advance(tyche_state* state){
+	state->a += state->b;
+	state->d = TYCHE_ROT(state->d ^ state->a, 16);
+	state->c += state->d;
+	state->b = TYCHE_ROT(state->b ^ state->c, 12);
+	state->a += state->b;
+	state->d = TYCHE_ROT(state->d ^ state->a, 8);
+	state->c += state->d;
+	state->b = TYCHE_ROT(state->b ^ state->c, 7);
+}
+
+void tyche_seed(tyche_state* state, ulong seed){
+	state->a = seed >> 32;
+	state->b = seed;
+	state->c = 2654435769;
+	state->d = 1367130551 ^ (get_global_id(0) + get_global_size(0) * (get_global_id(1) + get_global_size(1) * get_global_id(2)));
+	for(uint i=0;i<20;i++){
+		tyche_advance(state);
+	}
+}
+
+#define tyche_uint(state) ((uint)tyche_ulong(state))
+
+int main(int argc, char **argv) {
+    cl_event          event = NULL;
+    cl_int            err = -1;
+
+    oclStruct* tmpStructPtr = (oclStruct*) malloc(sizeof(oclStruct));
+    int res = makeOclStruct(argc, argv, tmpStructPtr);
+    if (res != 0) {
+        fprintf(stderr,"Unable to create oclStruct!\n");
+        return res;
+    }
+    res = printOclStructInfo(tmpStructPtr);
+    if (res != 0) {
+        fprintf(stderr,"Unable to print information about oclStruct!\n");
+        return res;
+    }
+
+    cl_device_id tmpDev = (*tmpStructPtr).target_device;
+    (*tmpStructPtr).ctx = clCreateContext(NULL, 1, &tmpDev, NULL, NULL, &err);
+    if (err) {
+        std::cout << "ERROR: unable to create context to extract random uint!" << std::endl;
+        return -1;
+    }
+
+    clRAND* test = clrand_create_stream();
+    clrand_initialize_prng(test, (*tmpStructPtr).target_device, (*tmpStructPtr).ctx, CLRAND_GENERATOR_TYCHE);
+    (*tmpStructPtr).queue = test->GetStreamQueue();
+
+    err = test->SetupWorkConfigurations();
+    if (err) {
+        fprintf(stderr,"Unable to set the execution configuration!\n");
+        return -1;
+    }
+
+    // Initialize the counters that tracks available random number generators
+    size_t numPRNGs = test->GetNumberOfRNGs();
+    size_t bufMult = 1;
+
+    err = test->SetupStreamBuffers(bufMult, numPRNGs);
+    test->SetReady();
+
+    // Seed the RNGs
+    err = test->SeedGenerator();
+    if (err != 0) {
+        std::cout << "ERROR: failed to seed PRNG" << std::endl;
+        return -1;
+    }
+
+    size_t stateStructSize = test->GetStateStructSize();
+    size_t stateMemSize = test->GetStateBufferSize();
+    // Prepare host memory to copy RNG states from device to host
+    tyche_state* state_mem = new tyche_state[numPRNGs];
+    if (stateMemSize == numPRNGs * sizeof(tyche_state)) {
+        err = test->CopyStateToHost((void*)(state_mem));
+        if (err) {
+            std::cout << "ERROR: unable to copy state buffer to host!" << std::endl;
+        }
+    } else {
+        std::cout << "ERROR: something went wrong setting up memory sizes!" << std::endl;
+        std::cout << "State Structure Size (host side): " << sizeof(isaac_state) << std::endl;
+        std::cout << "State Structure Size (obj side): " << stateStructSize << std::endl;
+        std::cout << "Number of PRNGs: " << numPRNGs << std::endl;
+        std::cout << "Size of state buffer: " << stateMemSize << std::endl;
+    }
+
+    // Generate RNG states on host side
+    tyche_state* golden_states = new tyche_state[numPRNGs];
+    ulong init_seedVal = test->GetSeed();
+    uint err_counts = 0;
+    for (int idx = 0; idx < numPRNGs; idx++) {
+        ulong newSeed = (ulong)(idx);
+        newSeed <<= 1;
+        newSeed += init_seedVal;
+        if (newSeed == 0) {
+            newSeed += 1;
+        }
+        tyche_seed(&golden_states[idx], newSeed);
+        if (golden_states[idx].res != state_mem[idx].res) {
+            err_counts++;
+            std::cout << "Mismatch in res at idx = " << idx << std::endl;
+            continue;
+        }
+    }
+    if (err_counts == 0) {
+        std::cout << "No errors detected after seeding!" << std::endl;
+    } else {
+        std::cout << err_counts << " errors detected after seeding!" << std::endl;
+        return -2;
+    }
+
+    std::cout << "Attempting to generate random uint on device..." << std::endl;
+    err = test->FillBuffer();
+    if (err) {
+        std::cout << "ERROR: unable to fill temporary buffer with random numbers" << std::endl;
+        return err;
+    }
+
+    uint* deviceRandomNumbers = new uint[numPRNGs];
+    cl_mem deviceRandomBuffer = clCreateBuffer((*tmpStructPtr).ctx, CL_MEM_READ_WRITE, test->GetNumBufferEntries() * sizeof(uint), NULL, &err);
+    if (err) {
+        std::cout << "ERROR: unable to create buffer to extract random uint!" << std::endl;
+        return err;
+    }
+    
+    err = test->CopyBufferEntries(deviceRandomBuffer, 0, test->GetNumBufferEntries());
+    if (err) {
+        std::cout << "ERROR: unable to perform buffer-to-buffer copy to extract random uint!" << std::endl;
+        return err;
+    }
+    err = clEnqueueReadBuffer((*tmpStructPtr).queue, deviceRandomBuffer, true, 0, test->GetNumBufferEntries() * sizeof(uint), deviceRandomNumbers, 0, NULL, &event);
+    if (err) {
+        std::cout << "ERROR: unable to enqueue read buffer to extract random uint!" << std::endl;
+        return err;
+    }
+    err = clWaitForEvents(1, &event);
+    if (err) {
+        std::cout << "ERROR: unable to wait for reading buffer to extract random uint!" << std::endl;
+        return err;
+    }
+    std::cout << "Attempting to generate random uint on host..." << std::endl;
+
+    err_counts = 0;
+    uint* hostRandomNumbers = new uint[numPRNGs];
+    for (int idx = 0; idx < numPRNGs; idx++) {
+        hostRandomNumbers[idx] = tyche_uint(golden_states[idx]);
+        if (hostRandomNumbers[idx] != deviceRandomNumbers[idx]) {
+            std::cout << "ERROR: numbers do not match at idx = " << idx << std::endl;
+            err_counts++;
+        }
+    }
+    if (err_counts == 0) {
+        std::cout << "No errors detected after random number generation!" << std::endl;
+    } else {
+        std::cout << err_counts << " errors detected after seeding!" << std::endl;
+        return -2;
+    }
+   
+    // Completed checks...
+    std::cout << "Checks completed!..." << std::endl;
+    delete [] state_mem;
+    delete [] golden_states;
+    delete [] deviceRandomNumbers;
+    delete [] hostRandomNumbers;
+    free(tmpStructPtr);
+    return res;
+}
