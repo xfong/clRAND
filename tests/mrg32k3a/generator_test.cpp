980696d4460a6cd5fdbd384ab8f6acf23ca1326e
Xuanyao Fong <xuanyao.fong@gmail.com>
2020-01-30 08:11:13 +0800

    Added mrg32k3a prng

---
+++ tests/mrg32k3a/generator_test.cpp
@@ -0,0 +1,214 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "../../src/clrand.hpp"
+#include "utils.h"
+
+#define __CL_ENABLE_EXCEPTIONS
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+
+#if defined(__APPLE__) || defined(__MACOSX)
+    #include <OpenCL/cl.h>
+#else
+    #include <CL/cl.h>
+#endif
+
+#define MRG32K3A_M1 4294967087
+#define MRG32K3A_M2 4294944443
+
+#define mrg32k3a_uint(state) (uint)_mrg32k3a_ulong(&state)
+#define mrg32k3a_ulong(state) _mrg32k3a_ulong(&state)
+
+void mrg32k3a_seed(mrg32k3a_state* state, ulong j){
+    ulong* g1 = state->g1;
+    ulong* g2 = state->g2;
+    g1[0] = j % MRG32K3A_M1;
+    g1[1] = 1;
+    g1[2] = 1;
+    g2[0] = 1;
+    g2[1] = 1;
+    g2[2] = 1;
+}
+
+ulong _mrg32k3a_ulong(mrg32k3a_state* state){
+
+    ulong* g1 = state->g1;
+    ulong* g2 = state->g2;
+    long p0, p1;
+    
+    /* component 1 */
+    p0 = 1403580 * state->g1[1] - 810728 * state->g1[0];
+    p0 %= MRG32K3A_M1;
+    if (p0 < 0)
+        p0 += MRG32K3A_M1;
+    g1[0] = g1[1];
+    g1[1] = g1[2];
+    g1[2] = p0;
+
+    /* component 2 */
+    p1 = 527612 * g2[2] - 1370589 * g2[0];
+    p1 %= MRG32K3A_M2;
+    if (p1 < 0)
+        p1 += MRG32K3A_M2;
+    g2[0] = g2[1];
+    g2[1] = g2[2];
+    g2[2] = p1;
+
+    return (p0 - p1) + (p0 <= p1 ? MRG32K3A_M1 : 0);
+}
+
+int main(int argc, char **argv) {
+    cl_event          event = NULL;
+    cl_int            err = -1;
+
+    oclStruct* tmpStructPtr = (oclStruct*) malloc(sizeof(oclStruct));
+    int res = makeOclStruct(argc, argv, tmpStructPtr);
+    if (res != 0) {
+        fprintf(stderr,"Unable to create oclStruct!\n");
+        return res;
+    }
+    res = printOclStructInfo(tmpStructPtr);
+    if (res != 0) {
+        fprintf(stderr,"Unable to print information about oclStruct!\n");
+        return res;
+    }
+
+    cl_device_id tmpDev = (*tmpStructPtr).target_device;
+    (*tmpStructPtr).ctx = clCreateContext(NULL, 1, &tmpDev, NULL, NULL, &err);
+    if (err) {
+        std::cout << "ERROR: unable to create context to extract random uint!" << std::endl;
+        return -1;
+    }
+
+    clRAND* test = clrand_create_stream();
+    clrand_initialize_prng(test, (*tmpStructPtr).target_device, (*tmpStructPtr).ctx, CLRAND_GENERATOR_MRG32K3A);
+    (*tmpStructPtr).queue = test->GetStreamQueue();
+
+    err = test->SetupWorkConfigurations();
+    if (err) {
+        fprintf(stderr,"Unable to set the execution configuration!\n");
+        return -1;
+    }
+
+    // Initialize the counters that tracks available random number generators
+    size_t numPRNGs = test->GetNumberOfRNGs();
+    size_t bufMult = 1;
+
+    err = test->SetupStreamBuffers(bufMult, numPRNGs);
+    test->SetReady();
+
+    // Seed the RNGs
+    err = test->SeedGenerator();
+    if (err != 0) {
+        std::cout << "ERROR: failed to seed PRNG" << std::endl;
+        return -1;
+    }
+
+    size_t stateStructSize = test->GetStateStructSize();
+    size_t stateMemSize = test->GetStateBufferSize();
+    // Prepare host memory to copy RNG states from device to host
+    mrg32k3a_state* state_mem = new mrg32k3a_state[numPRNGs];
+    if (stateMemSize == numPRNGs * sizeof(mrg32k3a_state)) {
+        err = test->CopyStateToHost((void*)(state_mem));
+        if (err) {
+            std::cout << "ERROR: unable to copy state buffer to host!" << std::endl;
+        }
+    } else {
+        std::cout << "ERROR: something went wrong setting up memory sizes!" << std::endl;
+        std::cout << "State Structure Size (host side): " << sizeof(isaac_state) << std::endl;
+        std::cout << "State Structure Size (obj side): " << stateStructSize << std::endl;
+        std::cout << "Number of PRNGs: " << numPRNGs << std::endl;
+        std::cout << "Size of state buffer: " << stateMemSize << std::endl;
+    }
+
+    // Generate RNG states on host side
+    mrg32k3a_state* golden_states = new mrg32k3a_state[numPRNGs];
+    ulong init_seedVal = test->GetSeed();
+    uint err_counts = 0;
+    for (int idx = 0; idx < numPRNGs; idx++) {
+        ulong newSeed = (ulong)(idx);
+        newSeed <<= 1;
+        newSeed += init_seedVal;
+        if (newSeed == 0) {
+            newSeed += 1;
+        }
+        mrg32k3a_seed(&golden_states[idx], newSeed);
+        for (uint idx1 = 0; idx1 < 3; idx1++) {
+            if (golden_states[idx].g1[idx1] != state_mem[idx].g1[idx1]) {
+                err_counts++;
+                std::cout << "Mismatch in g1 at idx = " << idx << ", in :" << idx1 << std::endl;
+                continue;
+            }
+        }
+        for (uint idx1 = 0; idx1 < 3; idx1++) {
+            if (golden_states[idx].g2[idx1] != state_mem[idx].g2[idx1]) {
+                err_counts++;
+                std::cout << "Mismatch in g2 at idx = " << idx << ", in :" << idx1 << std::endl;
+                continue;
+            }
+        }
+    }
+    if (err_counts == 0) {
+        std::cout << "No errors detected after seeding!" << std::endl;
+    } else {
+        std::cout << err_counts << " errors detected after seeding!" << std::endl;
+        return -2;
+    }
+
+    std::cout << "Attempting to generate random uint on device..." << std::endl;
+    err = test->FillBuffer();
+    if (err) {
+        std::cout << "ERROR: unable to fill temporary buffer with random numbers" << std::endl;
+        return err;
+    }
+
+    uint* deviceRandomNumbers = new uint[numPRNGs];
+    cl_mem deviceRandomBuffer = clCreateBuffer((*tmpStructPtr).ctx, CL_MEM_READ_WRITE, test->GetNumBufferEntries() * sizeof(uint), NULL, &err);
+    if (err) {
+        std::cout << "ERROR: unable to create buffer to extract random uint!" << std::endl;
+        return err;
+    }
+    
+    err = test->CopyBufferEntries(deviceRandomBuffer, 0, test->GetNumBufferEntries());
+    if (err) {
+        std::cout << "ERROR: unable to perform buffer-to-buffer copy to extract random uint!" << std::endl;
+        return err;
+    }
+    err = clEnqueueReadBuffer((*tmpStructPtr).queue, deviceRandomBuffer, true, 0, test->GetNumBufferEntries() * sizeof(uint), deviceRandomNumbers, 0, NULL, &event);
+    if (err) {
+        std::cout << "ERROR: unable to enqueue read buffer to extract random uint!" << std::endl;
+        return err;
+    }
+    err = clWaitForEvents(1, &event);
+    if (err) {
+        std::cout << "ERROR: unable to wait for reading buffer to extract random uint!" << std::endl;
+        return err;
+    }
+    std::cout << "Attempting to generate random uint on host..." << std::endl;
+
+    err_counts = 0;
+    uint* hostRandomNumbers = new uint[numPRNGs];
+    for (int idx = 0; idx < numPRNGs; idx++) {
+        hostRandomNumbers[idx] = mrg32k3a_uint(golden_states[idx]);
+        if (hostRandomNumbers[idx] != deviceRandomNumbers[idx]) {
+            std::cout << "ERROR: numbers do not match at idx = " << idx << std::endl;
+            err_counts++;
+        }
+    }
+    if (err_counts == 0) {
+        std::cout << "No errors detected after random number generation!" << std::endl;
+    } else {
+        std::cout << err_counts << " errors detected after seeding!" << std::endl;
+        return -2;
+    }
+   
+    // Completed checks...
+    std::cout << "Checks completed!..." << std::endl;
+    delete [] state_mem;
+    delete [] golden_states;
+    delete [] deviceRandomNumbers;
+    delete [] hostRandomNumbers;
+    free(tmpStructPtr);
+    return res;
+}
